eb7f88b5c81a3af80bf2119de792bc4578c361c2
rkstavenuiter <rkst@rd8.ai>
2024-12-01 21:10:03 +0100

    2024-12-01

---
+++ 2024/1/solution.hs
@@ -0,0 +1,24 @@
+import System.IO  
+import Control.Monad
+import Data.List
+
+main = do  
+        let list = []
+        handle <- openFile "input.txt" ReadMode
+        contents <- hGetContents handle
+        let singlewords = words contents
+            list = f singlewords
+        let left = [list !! i | i <- [0..((length list) - 1)], even i]
+        let right = [list !! i | i <- [0..((length list) - 1)], odd i]
+
+        let res = sum . map (\(x, y) -> abs (x - y)) . zip (sort left) $ (sort right)
+        print res
+        
+        let similarities = [ (left !! i, length . filter (== (left !! i)) $ right) | i <- [0..((length left) - 1)] ]
+        let res2 = sum . map (\(x, y) -> x*y) $ similarities
+        print res2
+        hClose handle
+
+
+f :: [String] -> [Int]
+f = map read
